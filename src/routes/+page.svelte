018c6a3abcb7456477803b86a3860cdb10de4d87
IbrahimAhmad77977 <ibrahimahmad77977@gmail.com>
2024-08-29 21:22:37 +0500

    lsl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,339 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- add a background image -->
+<div class="section1">
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div
+      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+    >
+      <a
+        href="https://flowbite.com/"
+        class="flex items-center space-x-3 rtl:space-x-reverse"
+      >
+        <img
+          src="home/logo.png"
+          class="h-14 w-11 mt-4 ml-40"
+          alt="School logo"
+        />
+      </a>
+      <button
+        data-collapse-toggle="navbar-default"
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default"
+        aria-expanded="false"
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+      <div
+        class="hidden w-full md:block md:w-auto mr-[200px]"
+        id="navbar-default"
+      >
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 text-[#cecdcd]
+"
+        >
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500 h-4 w-11 font-serif font-bold text-sm tracking-[0.2px] text-[#cecdcd]"
+              aria-current="page">Home</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent h-4 w-10 font-serif font-normal text-sm tracking-[0.2px] text-[#cecdcd]
+">About</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent h-4 w-9 font-serif font-normal text-sm tracking-[0.2px] text-[#cecdcd]
+">Guru</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent h-4 w-9 font-serif font-normal text-sm tracking-[0.2px] text-[#cecdcd]
+">Siswa</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent h-4 w-20 font-serif font-normal text-sm tracking-[0.2px] text-[#cecdcd]
+">Pendaftaran</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent h-4 w-10 font-serif font-normal text-sm tracking-[0.2px] text-[#cecdcd]
+">lainya</a
+            >
+          </li>
+          <li>
+            <a
+              href="#"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent h-4 w-12 font-serif font-normal text-sm tracking-[0.2px] text-[#cecdcd]
+">Kontak</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div>
+    <h2
+      class="w-14 h-4 mt-40 ml-40 opacity-60 font-serif font-bold text-sm tracking-[0.4px] text-white"
+    >
+      MOTTO
+    </h2>
+    <p
+      class="w-(697px) h-28 mt-12 ml-40 font-serif font-bold text-2xl tracking-[0.2px] text-white"
+    >
+      SMP N 1 Cibadak (Cerdas Beretika) Ceria, Empati,<br /> Rasional, Damai,
+      Aktif, Sabar, Bersih, Elok, Tulus, Iman,<br /> Konsiste, Amanah.
+    </p>
+    <button
+      type="button"
+      class="text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-36 h-10 ml-40 mt-12 rounded color"
+      >Contact us</button
+    >
+  </div>
+</div>
+<!-- adjust it into a flex div -->
+<div class="flex flex-row">
+  <div class="flex p-2">
+    <div class="flex flex-col">
+      <div class="flex p-2">
+        <img
+          src="home/principal.png"
+          alt="principal"
+          width="439.71px"
+          class="h-80 ml-44 mt-10"
+        />
+      </div>
+
+      <img src="/home/circle.png" alt="circle" class="w-14 h-14 ml-96 circle" />
+      <img src="/home/play.png" alt="play" class="w-4 h-5 ml-96 play" />
+      <span class="dots"></span>
+      <span class="dots"></span>
+      <span class="dots"></span>
+      <span class="dots"></span>
+      <span class="dots"></span>
+      <span class="dots"></span>
+    </div>
+  </div>
+  <div class="flex p-2">
+    <div class="flex flex-col flexbox">
+      <div class="flex p-2">
+        <h1
+          class="w-96 h-24 heading1 font-serif font-medium text-3xl tracking-[0.3px]"
+        >
+          Sambutan Kepala Sekolah SMP Negeri 1 Cibadak
+        </h1>
+      </div>
+      <div class="flex p-2">
+        <p
+          class="h-16 font-seif font-bold text-xs tracking-[0.3px] paragraph1 mt-10"
+        >
+          Puji dan syukur mari kita panjatkan kehadirat Allah SWT. Yang
+          senantiasa dengan sifat kasih dan sayangnya banyak memberikan nikmat
+          ...
+        </p>
+      </div>
+      <div class="flex p-2">
+        <button
+          type="button"
+          class="text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-44 h-10 m-t 10 rounded color button1"
+          >Lebih Lanjut</button
+        >
+      </div>
+    </div>
+  </div>
+</div>
+<!-- adjust it into a flex div -->
+<div class="d-flex flex-row">
+  <div class="d-flex p-2">
+    <h1
+      class="w-44 h-9 mt-10 ml-44 font-serif font-bold text-xl tracking-[0.3px] heading2"
+    >
+      Profil Sekolah
+    </h1>
+    <p
+      class="w-96 h-24 mt-10 ml-44 font-serif font-bold text-sm tracking-[0.5px] paragraph2"
+    >
+      Di samping adalah profil sekolah kami secara keseluruhan dari mulai bagian
+      depan hingga seluruh fasilitas yang terdapat disekolah kami kami akan ...
+    </p>
+    <button
+      type="button"
+      class="text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-44 h-10 m-t 10 ml-44 rounded color"
+      >Lebih Lanjut</button
+    >
+  </div>
+  <div class="d-flex p-2">
+    <img src="home/house.png" alt="house" class="w-4 h-4 mt-10 house" />
+    <img src="home/dot1.png" alt="dot1" class="w-2 h-2 border-2" />
+    <img src="home/dot2.png" alt="dot2" class="w-2 h-2 border-2" />
+    <img src="home/dot3.png" alt="dot3" class="w-2 h-2 border-2" />
+    <img src="home/dot4.png" alt="dot4" class="w-2 h-2 border-2" />
+    <img src="home/dot5.png" alt="dot5" class="w-2 h-2 border-2" />
+    <img src="home/dot6.png" alt="dot6" class="w-2 h-2 border-2" />
+    <img src="home/circle2.png" alt="circle2" class="w-9 h-9 circle2" />
+
+    <h1
+      class="w-20 h-7 mt-10 font-serif font-bold text-xl tracking-[0.3px] card1"
+    >
+      Fasilitas
+    </h1>
+    <p
+      class="w-56 h-12 mt-10 card2 font-serif font-normal text-xs tracking-[0.3px]"
+    >
+      Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+    </p>
+  </div>
+  <img src="home/paper.png" alt="paper" class="w-10 h-10" />
+  <h1
+    class="w-20 h-7 mt-10 font-serif font-bold text-xl card1 tracking-[0.3px]"
+  >
+    Sejarah
+  </h1>
+  <p
+    class="w-56 h-12 mt-10 card2 font-serif font-normal text-xs tracking-[0.3px]"
+  >
+    Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+  </p>
+  <div class="d-flex p-2">
+    <h1
+      class="w-16 h-7 mt-10 font-serif font-bold text-xl tracking-[0.3px] card3"
+    >
+      Lokasi
+    </h1>
+    <p
+      class="w-56 h-12 mt-10 font-serif font-normal text-xs tracking-[0.3px] card4"
+    >
+      Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+    </p>
+    <img src="home/badge.png" alt="badge" class="w-9 h-11 border-2:" />
+    <h1
+      class="w-20 h-7 mt-10 font-serif font-bold text-xl tracking-[0.3px] card3"
+    >
+      Prestasi
+    </h1>
+    <p
+      class="w-56 h-12 mt-10 font-serif font-normal text-xs tracking-[0.3px] card4"
+    >
+      Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+    </p>
+  </div>
+</div>
+
+<style>
+  .color {
+    background-color: #0193dc;
+  }
+  .section1 {
+    background-image: url ("home/background.png");
+    background-color: #000000;
+    opacity: 70%;
+  }
+  .dots {
+    height: 10px;
+    width: 10px;
+    background-color: #0193dc;
+    border-radius: 50%;
+    display: inline-block;
+  }
+  .heading1 {
+    margin-left: 290px;
+    margin-top: 60px;
+    color: #0c101a;
+  }
+  .paragraph1 {
+    margin-left: 290px;
+    color: #919fae;
+  }
+  .button1 {
+    margin-left: 290px;
+  }
+  .circle {
+    background-color: #ffffff;
+    z-index: 2;
+  }
+  .heading2 {
+    color: #0c101a;
+  }
+  .paragraph2 {
+    color: #919fae;
+  }
+  .card1 {
+    margin-left: 732px;
+    color: #0c101a;
+  }
+  .card2 {
+    margin-left: 732px;
+    color: #919fae;
+  }
+  .card3 {
+    margin-left: 1022px;
+    color: #0c101a;
+  }
+  .card4 {
+    margin-left: 1022px;
+    color: #919fae;
+  }
+  .house {
+    margin-left: 756.45px;
+    color: #60c1f8;
+  }
+  .yellow {
+    color: #f8c301;
+  }
+  .circle2 {
+    margin-left: 744px;
+  }
+  .dot1 {
+    margin-left: 760.26px;
+  }
+  .dot2 {
+    margin-left: 744px;
+  }
+  .dot3 {
+    margin-left: 750.61px;
+  }
+  .dot4 {
+    margin-left: 760.26px;
+  }
+  .dot5 {
+    margin-left: 773.73px;
+  }
+  .dot6 {
+    margin-left: 780.34px;
+  }
+  .circle {
+    background-color: green;
+  }
+  .play {
+    background-color: green;
+  }
+  .flexbox {
+  }
+</style>
