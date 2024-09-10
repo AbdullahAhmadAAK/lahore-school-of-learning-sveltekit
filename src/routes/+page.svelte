cd619643cceb2e6dcb251f4b215791331c3cde5d
IbrahimAhmad77977 <ibrahimahmad77977@gmail.com>
2024-09-10 15:38:38 +0500

    new changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -186,8 +186,7 @@
   </div>
 </div>
 
-<!-- adjust it into a flex div -->
-<div class="flex flex-row">
+<div class="flex flex-row bg-[#F9FBFD]">
   <div class="flex flex-col p-2">
     <h1
       class="w-44 h-9 mt-10 ml-44 font-serif font-bold text-xl tracking-[0.3px] text-[#0c101a]
@@ -208,57 +207,476 @@
       >Lebih Lanjut</button
     >
   </div>
-  <div class="flex flex-col p-2">
-    <h1
-      class="w-20 h-7 font-serif font-bold text-xl tracking-[0.3px] text-[#0c101a]"
-    >
-      Fasilitas
-    </h1>
-    <p
-      class="w-56 h-12 mt-10 text-[#919fae]
-  font-serif font-normal text-xs tracking-[0.3px]"
-    >
-      Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
-    </p>
-  </div>
-  <div class="flex p-2">
-    <h1
-      class="w-20 h-7 mt-10 font-serif font-bold text-xl text-[#0c101a]
-  tracking-[0.3px]"
-    >
-      Sejarah
-    </h1>
-    <p
-      class="w-56 h-12 mt-10 card2 font-serif font-normal text-xs tracking-[0.3px]"
-    >
-      Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
-    </p>
-  </div>
-  <div class="flex p-2">
-    <h1
-      class="w-16 h-7 mt-10 font-serif font-bold text-xl tracking-[0.3px] text-[#0c101a]"
-    >
-      Lokasi
-    </h1>
-    <p
-      class="w-56 h-12 mt-10 font-serif font-normal text-xs tracking-[0.3px] text-[#919fae]"
-    >
-      Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
-    </p>
-    <div class="d-flex p-2">
+  <div class="flex flex-col">
+    <div class="flex flex-col mt-10 p-2 border-black border-2">
+      <img src="home/house.png" alt="house" class="w-4 h-4 ml-4 mb-4" />
       <h1
-        class="w-20 h-7 mt-10 font-serif font-bold text-xl tracking-[0.3px] text-[#0c101a]"
+        class="w-20 h-7 font-serif font-bold text-xl tracking-[0.3px] text-[#0c101a]"
+      >
+        Fasilitas
+      </h1>
+      <p
+        class="w-56 h-12 mt-10 text-[#919fae]
+  font-serif font-normal text-xs tracking-[0.3px]"
+      >
+        Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+      </p>
+    </div>
+    <div class="flex flex-col p-2 border-black border-2">
+      <img src="home/paper.png" alt="house" class="w-8 h-8 ml-4" />
+
+      <h1
+        class="w-20 h-7 mt-10 font-serif font-bold text-xl text-[#0c101a]
+  tracking-[0.3px]"
+      >
+        Sejarah
+      </h1>
+      <p
+        class="w-56 h-12 mt-10 card2 font-serif font-normal text-xs tracking-[0.3px]"
+      >
+        Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+      </p>
+    </div>
+  </div>
+  <div class="flex flex-col">
+    <div class="flex flex-col mt-10 p-2 border-black border-2">
+      <img src="home/location.png" alt="house" class="w-4 h-4 ml-4 mb-4" />
+      <h1
+        class="w-20 h-7 font-serif font-bold text-xl tracking-[0.3px] text-[#0c101a]"
+      >
+        Lokasi
+      </h1>
+      <p
+        class="w-56 h-12 mt-10 text-[#919fae]
+  font-serif font-normal text-xs tracking-[0.3px]"
+      >
+        Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
+      </p>
+    </div>
+    <div class="flex flex-col p-2 border-black border-2">
+      <img src="home/badge.png" alt="house" class="w-8 h-8 ml-4" />
+
+      <h1
+        class="w-20 h-7 mt-10 font-serif font-bold text-xl text-[#0c101a]
+  tracking-[0.3px]"
       >
         Prestasi
       </h1>
       <p
-        class="w-56 h-12 mt-10 font-serif font-normal text-xs tracking-[0.3px] card4 ml-[1022px]"
+        class="w-56 h-12 mt-10 card2 font-serif font-normal text-xs tracking-[0.3px]"
       >
         Lorem ipsum dolor sit amet adipcing aqua lorem ipsum.
       </p>
     </div>
   </div>
 </div>
+<div class="flex flex-row">
+  <div class="flex p-2">
+    <h1
+      class="w-56 h-9 mt-20 ml-40 font-serif font-bold text-xl tracking-[0.3px]"
+    >
+      Berita dan Agenda
+    </h1>
+    <div class="flex p-2">
+      <form class="max-w-md mx-auto">
+        <label
+          for="default-search"
+          class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >Search</label
+        >
+        <div class="relative">
+          <div
+            class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
+          >
+            <svg
+              class="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            class="block w-full p-4 ps-10 text-sm text-gray-900 rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-serif font-normal tracking-[0.3px] mt-14"
+            placeholder="Search"
+            required
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="flex flex-row">
+  <div class="flex flex-row p-2">
+    <div class="flex flex-col p-2">
+      <img
+        src="home/learning.png"
+        alt="learning"
+        class="w-44 h-40 ml-44 mt-10"
+      />
+      <img
+        src="home/classroom.png"
+        alt="classroom"
+        class="w-44 h-40 ml-44 mt-10"
+      />
+      <img
+        src="home/students.png"
+        alt="students"
+        class="w-44 h-40 ml-44 mt-10"
+      />
+      <img src="home/online.png" alt="online" class="w-44 h-40 ml-44 mt-10" />
+    </div>
+    <div class="flex flex-col p-2">
+      <h1
+        class="w-36 h-5 mt-10 font-serif font-bold text-sm tracking-[0.3px] ml-24"
+      >
+        Belajar di Rumah
+      </h1>
+      <p
+        class="w-[370px] h-12 mt-4 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+      >
+        Lorem ipsum dolor sit amet adipcing amet adipcingamet adipci aqua lorem
+        ipsum.
+      </p>
+      <p
+        class="w-11 h-6 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+      >
+        20 Juni
+      </p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-4 h-4 ml-[510px]"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+        />
+      </svg>
+      <div class="flex flex-row">
+        <div class="flex flex-col p-2">
+          <h1
+            class="w-60 h-11 mt-16 font-serif font-bold text-base tracking-[0.3px] ml-24"
+          >
+            Kegiatan Belajar mengajar di Rumah
+          </h1>
+          <p
+            class="w-[370px] h-12 mt-4 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+          >
+            Lorem ipsum dolor sit amet adipcing amet adipcingamet adipci aqua
+            lorem ipsum.
+          </p>
+          <p
+            class="w-11 h-6 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+          >
+            20 Juni
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4 ml-[510px]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="flex flex-row">
+        <div class="flex flex-col p-2">
+          <h1
+            class="w-60 h-11 mt-10 font-serif font-bold text-base tracking-[0.3px] ml-24"
+          >
+            Kegiatan Belajar mengajar di Rumah
+          </h1>
+          <p
+            class="w-[370px] h-12 mt-4 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+          >
+            Lorem ipsum dolor sit amet adipcing amet adipcingamet adipci aqua
+            lorem ipsum.
+          </p>
+          <p
+            class="w-11 h-6 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+          >
+            20 Juni
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4 ml-[510px]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="flex flex-row">
+        <div class="flex flex-col p-2">
+          <h1
+            class="w-64 h-5 mt-10 font-serif font-bold text-base tracking-[0.3px] ml-24"
+          >
+            Kegiatan Pembelajaran Daring
+          </h1>
+          <p
+            class="w-[370px] h-12 mt-10 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+          >
+            Lorem ipsum dolor sit amet adipcing amet adipcingamet adipci aqua
+            lorem ipsum.
+          </p>
+          <p
+            class="w-11 h-6 font-serif font-normal text-xs tracking-[0.3px] text-[#919FAE] ml-24"
+          >
+            20 Juni
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-4 h-4 ml-[510px]"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="flex flex-col">
+    <div class="flex flex-row">
+      <div class="flex p-2">
+        <img src="home/logo.png" alt="logo" class="w-8 h-16 mt-10 ml-10" />
+      </div>
+      <div class="flex flex-col p-2">
+        <p
+          class="w-24 h-6 mt-10 font-serif font-normal text-sm tracking-[0.3px] text-[#919FAE] ml-4"
+        >
+          01 - 06 - 2021
+        </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-4 h-4 ml-72"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+          />
+        </svg>
+
+        <h1
+          class="w-72 h-5 font-serif font-bold text-base tracking-[0.3px] ml-4"
+        >
+          Deklarasi Sekolah Ramah Anak
+        </h1>
+      </div>
+    </div>
+    <div class="flex flex-row">
+      <div class="flex p-2">
+        <img src="home/logo.png" alt="logo" class="w-8 h-16 mt-10 ml-10" />
+      </div>
+      <div class="flex flex-col p-2">
+        <p
+          class="w-24 h-6 mt-10 font-serif font-normal text-sm tracking-[0.3px] text-[#919FAE] ml-4"
+        >
+          01 - 06 - 2021
+        </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-4 h-4 ml-72"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+          />
+        </svg>
+
+        <h1
+          class="w-40 h-5 font-serif font-bold text-base tracking-[0.3px] ml-4"
+        >
+          Pembagian Ijazah
+        </h1>
+      </div>
+    </div>
+    <div class="flex flex-row">
+      <div class="flex p-2">
+        <img src="home/study.png" alt="study" class="w-10 h-14 mt-10 ml-10" />
+      </div>
+      <div class="flex flex-col p-2">
+        <p
+          class="w-24 h-6 mt-10 font-serif font-normal text-sm tracking-[0.3px] text-[#919FAE] ml-8"
+        >
+          01 - 06 - 2021
+        </p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-4 h-4 ml-[300px]"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+          />
+        </svg>
+
+        <h1
+          class="w-72 h-5 font-serif font-bold text-base tracking-[0.3px] ml-8"
+        >
+          Pemberitahuan PPDB 2020/2021
+        </h1>
+      </div>
+    </div>
+  </div>
+</div>
+<h1 class="w-20 h-9 ml-52 mt-24 font-serif font-bold text-2xl tracking-[0.3px]">
+  Gallery
+</h1>
+<div class="flex flex-row gap-8">
+  <img src="home/study2.png" alt="study2" class="w-80 h-72 ml-48" />
+  <img src="home/library.png" alt="library" class="w-80 h-72" />
+  <img src="home/study3.png" alt="study3" class="w-80 h-72" />
+</div>
+<div class="flex flex-row gap-8 mt-8">
+  <img src="home/study4.png" alt="study4" class="w-80 h-72 ml-48" />
+  <img src="home/study5.png" alt="study5" class="w-80 h-72" />
+  <img src="home/study6.png" alt="study6" class="w-80 h-72" />
+</div>
+<button
+  type="button"
+  class="text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-normal text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-44 h-10 rounded bg-[#0193dc] ml-[610px] mt-10"
+  >Lebih Lanjut</button
+>
+<footer class="bg-[#F9F9F9]">
+  <div class="flex flex-row mt-40">
+    <div class="flex flex-col p-2 mt-10">
+      <img src="home/logo2.png" alt="logo2" class="w-14 h-16 ml-40" />
+      <p
+        class="w-72 h-12 mt-10 ml-40 font-serif font-normal text-xs tracking-[0.3px]"
+      >
+        Jl. Siliwangi No 123, Cibadak, Cibadak, Sukabumi, Jawa Barat 43351,
+        Indonesia, (0266)531333
+      </p>
+      <h2
+        class="w-40 h-6 mt-2 ml-40 font-serif font-bold text-xs tracking-[0.3px]"
+      >
+        info@smpn1cibadak.sch.id
+      </h2>
+      <h2
+        class="w-44 h-6 ml-40 mt-1 font-serif font-bold text-xs tracking-[0.3px]"
+      >
+        smpn1cbd_kabsi@yahoo.co.id
+      </h2>
+    </div>
+
+    <div class="flex flex-col p-2 gap-2 mt-10 ml-40">
+      <h2 class="w-14 h-5 font-serif font-bold text-base tracking-[0.3px]">
+        Jelajah
+      </h2>
+      <h2 class="w-20 h-4 font-serif font-bold text-sm tracking-[0.2px] mt-4">
+        Sambutan
+      </h2>
+      <h2 class="w-32 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        Profil Sekolah
+      </h2>
+      <h2 class="w-11 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        Berita
+      </h2>
+      <h2 class="w-11 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        Galeri
+      </h2>
+    </div>
+    <div class="flex flex-col p-2 gap-2 mt-10 ml-20">
+      <h2 class="w-36 h-6 font-serif font-bold text-base tracking-[0.3px]">
+        Halaman Umum
+      </h2>
+      <h2 class="w-20 h-4 font-serif font-bold text-sm tracking-[0.2px] mt-4">
+        Data Guru
+      </h2>
+      <h2 class="w-44 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        PPDB SMPN 1 Cibadak
+      </h2>
+      <h2 class="w-32 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        Panduan PPDB
+      </h2>
+      <h2 class="w-12 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        Lokasi
+      </h2>
+      <h2 class="w-14 h-4 font-serif font-bold text-sm tracking-[0.2px]">
+        Kontak
+      </h2>
+    </div>
+    <div class="flex flex-col p-2 gap-2 mt-10 ml-16">
+      <h2 class="w-28 h-5 font-serif font-bold text-base tracking-[0.3px]">
+        Media Sosial
+      </h2>
+      <div class="flex flex-row mt-4 gap-2">
+        <div class="flex p-2 border-2 rounded-lg border-[#E6E6E7] w-8 h-8">
+          <img src="home/twitter.png" alt="twitter" class="w-3.5 h-3" />
+        </div>
+        <div class="flex p-2 border-2 rounded-lg border-[#E6E6E7] w-8 h-8">
+          <img
+            src="home/facebook.png"
+            alt="facebook"
+            class="w-1.5 h-2.5 ml-[3px]"
+          />
+        </div>
+        <div class="flex p-2 border-2 rounded-lg border-[#E6E6E7] w-8 h-8">
+          <img src="home/instagram.png" alt="instagram" class="w-2.5 h-2.5" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <p
+      class="w-[460px] h-6 font-serif font-normal text-xs tracking-[0.3px] ml-[166px] mt-10"
+    >
+      Copyright &#169; SMP Negeri 1 Cibadak. All right Reserved. Hosting By
+      IDCloudHost
+    </p>
+  </div>
+</footer>
 
 <style>
   .section1 {
